069b8d79ca7eaccdc19f64479875323ad0a58933
Zehra Coskun <rzg>
2023-03-20 20:46:48 +0300

    Initial Commit

---
+++ iExpenses/iExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesApp.swift
+//  iExpenses
+//
+//  Created by Zehra Coşkun on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
